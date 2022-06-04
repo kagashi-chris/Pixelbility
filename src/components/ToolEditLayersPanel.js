00cac04f4c31432dde39cfc14fa21626c2e77bfe
Chris Zhen <chris.zhenartist@gmail.com>
2022-06-04 15:16:11 -0400

    updated toolEditLayerPanel css

--- src/components/ToolEditLayersPanel.js
+++ src/components/ToolEditLayersPanel.js
@@ -2,6 +2,12 @@ import React, { Component } from "react";
 
 export default class ToolEditLayersPanel extends Component {
   render() {
-    return <div className="toolEditLayersPanel"></div>;
+    return (
+      <div className="toolEditLayersPanel">
+        <button id="seperate-image">Seperate Image</button>
+        <div id="layer-container"></div>
+        <div id="layer-container-footer"></div>
+      </div>
+    );
   }
 }
